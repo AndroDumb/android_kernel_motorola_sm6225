3fbd599d186b2c4c3f4ded5cda011feb41849b8b
AndroDumb <torreszabdiel29@gmail.com>
2026-08-08 02:36:27 -0400

    sys.c: Remove unnecessary closing brace in sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1268,7 +1268,6 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 #endif
 		pr_debug("fake uname: %s/%d release=%s\n",
 			current->comm, current->pid, tmp.release);
-		}
 	}
 #endif
 	up_read(&uts_sem);
